4c6ee19539253217f94d8fc289a3cb62376ad605
kankan jana <csekankan@gmail.com>
2024-07-31 21:26:31 +0530

    commit all

--- bin/test1.js
+++ bin/test1.js
@@ -3,15 +3,16 @@
 const cdk = require('aws-cdk-lib');
 const lambda = require('aws-cdk-lib/aws-lambda');
 const apigateway = require('aws-cdk-lib/aws-apigateway');
-const { App, Stack } = cdk;
+const { App, Stack, CfnOutput } = cdk;
 
 class Test1Stack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // Define the Lambda function
-    const testGitFunction = new lambda.Function(this, 'test_git', {
-      runtime: lambda.Runtime.NODEJS_16_X, // Choose the runtime, for example, Node.js 14
+    // Define the Lambda function with a unique logical ID and user-defined name
+    const testGitFunction = new lambda.Function(this, 'TestGitFunction', { // Logical ID
+      functionName: 'test_git', // User-defined function name
+      runtime: lambda.Runtime.NODEJS_16_X, // Choose the runtime, for example, Node.js 16
       code: lambda.Code.fromInline(`exports.handler = async function(event) {
         return {
             statusCode: 200,
@@ -25,6 +26,12 @@ class Test1Stack extends Stack {
     new apigateway.LambdaRestApi(this, 'Endpoint', {
       handler: testGitFunction,
     });
+
+    // Output the Lambda function ARN for easy reference
+    new CfnOutput(this, 'LambdaFunctionArn', {
+      value: testGitFunction.functionArn,
+      description: 'ARN of the test_git Lambda function',
+    });
   }
 }
 
@@ -32,5 +39,4 @@ const app = new App();
 new Test1Stack(app, 'Test1Stack', {
   // Uncomment and modify if needed to specify the environment
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  // env: { account: '123456789012', region: 'us-east-1' },
 });
--- test.js
+++ test.js
@@ -2,32 +2,30 @@
 require('dotenv').config();
 // invokeLambda.js
 
-// Accessing environment variables from GitHub Actions
+
 console.log("TEST value is:", process.env.TEST || "Not set");
 console.log("SECRET value is:", process.env.SECRET || "Not set");
 
 const AWS = require('aws-sdk');
 
-// Create Lambda service object
+// Set the region
+AWS.config.update({ region: 'ap-south-1' });
+
 const lambda = new AWS.Lambda();
+const functionName = 'test_git'; // Use the user-defined function name
 
-// Define the name of the Lambda function
-const functionName = 'test_git'; // Use your Lambda function name
-
-// Prepare the parameters for the invocation
 const params = {
     FunctionName: functionName,
-    InvocationType: 'RequestResponse', // Synchronous invocation
-    Payload: JSON.stringify({ key: 'value' }) // Optional: Pass any data you want to send to the Lambda function
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({ key: 'value' }),
 };
 
 lambda.invoke(params, (error, data) => {
     if (error) {
         console.error('Error invoking Lambda function:', error);
     } else {
-        // Log the response from the Lambda function
         try {
-            const response = JSON.stringify(data.Payload); // Parse the payload which is a JSON string
+              const response = JSON.stringify(data.Payload); // Parse the payload which is a JSON string
             console.log('Lambda function response:', response);
         } catch (err) {
             console.error('Error parsing Lambda response:', err);
