e601bf07f203d34d8c7d4a2b48f7c9cd75356cf7
kankan jana <csekankan@gmail.com>
2024-07-31 20:55:52 +0530

    commit

--- bin/test1.js
+++ bin/test1.js
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib');
-const { Test1Stack } = require('../lib/test1-stack');
+const lambda = require('aws-cdk-lib/aws-lambda');
+const apigateway = require('aws-cdk-lib/aws-apigateway');
+const { App, Stack } = cdk;
 
-const app = new cdk.App();
+class Test1Stack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Define the Lambda function
+    const testGitFunction = new lambda.Function(this, 'test_git', {
+      runtime: lambda.Runtime.NODEJS_14_X, // Choose the runtime, for example, Node.js 14
+      code: lambda.Code.fromInline(`exports.handler = async function(event) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello, World!' }),
+        };
+      };`), // Inline function code
+      handler: 'index.handler', // The handler name for the inline code
+    });
+
+    // Create an API Gateway to expose the Lambda function
+    new apigateway.LambdaRestApi(this, 'Endpoint', {
+      handler: testGitFunction,
+    });
+  }
+}
+
+const app = new App();
 new Test1Stack(app, 'Test1Stack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+  // Uncomment and modify if needed to specify the environment
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
