f2cc14fe57c76c2a45b9ce3b60ebed9f9add1f66
kankan jana <csekankan@gmail.com>
2024-07-31 19:58:52 +0530

    commit

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
 // Accessing environment variables using process.env
 require('dotenv').config();
 
-console.log("MY_SECRET value is:", process.env.MY_SECRET);
+console.log("MY_SECRET value is:", process.env.TEST);
